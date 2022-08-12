602cacc4e4dac1cf407972707f93e18301cc7114
yechentide <yechentide@gmail.com>
2022-08-12 19:37:19 +0900

    fix: access modifier

--- Sources/CoreBedrock/Models/MCPlayer.swift
+++ Sources/CoreBedrock/Models/MCPlayer.swift
@@ -5,11 +5,11 @@ public struct MCPlayer: Equatable, Hashable {
     public let signedID: String    // player_d3663243-007c-33de-8bf1-384226d03329
     public let serverID: String    // player_server_de3a8a4b-a66f-465f-881b-0389e4993834
     
-    static func == (lhs: MCPlayer, rhs: MCPlayer) -> Bool {
+    public static func == (lhs: MCPlayer, rhs: MCPlayer) -> Bool {
         return lhs.msaID == rhs.msaID
     }
     
-    func hash(into hasher: inout Hasher) {
+    public func hash(into hasher: inout Hasher) {
         hasher.combine(msaID)
     }
 }
