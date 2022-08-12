26082b8da170be6d824a1ca6ae0825e4c4084bf9
yechentide <yechentide@gmail.com>
2022-08-12 14:51:54 +0900

    feat: new custom error enum

---
+++ Sources/CoreBedrock/CBError.swift
@@ -0,0 +1,15 @@
+public enum CBError: Error, Equatable {
+    case invalidReaderState(_ message: String)
+    case invalidFormat(_ message: String)
+    case invalidOperation(_ message: String)
+    case argumentError(_ message: String)
+    case argumentError(_ message: String, _ paramName: String)
+    case argumentOutOfRange(_ paramName: String, _ message: String)
+    case endOfStream
+    case invalidCast
+    case invalidData(_ message: String)
+    case compressionError
+    case stringConversionError
+    case seekBeforeBegin
+    case bufferNotExpandable
+}
