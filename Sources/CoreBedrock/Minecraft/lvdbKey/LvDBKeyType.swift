f6ac0aa15f11f24323c07292dc9ae7371c18a922
yechentide <yechentide@gmail.com>
2023-04-26 23:36:38 +0900

    feat: new computed property for levelDB key

--- Sources/CoreBedrock/Minecraft/lvdbKey/LvDBKeyType.swift
+++ Sources/CoreBedrock/Minecraft/lvdbKey/LvDBKeyType.swift
@@ -8,4 +8,22 @@ public enum LvDBKeyType {
     case actorprefix
     case digp
     case unknown
+
+    public var isNBT: Bool {
+        switch self {
+            case .string(let strType):
+                switch strType {
+                    case .localPlayer, .autonomousEntities, .biomeData,
+                            .levelChunkMetaDataDictionary, .mobevents,
+                            .overworld, .schedulerWT, .scoreboard:
+                        return true
+                    default:
+                        return false
+                }
+            case .player, .map, .village, .structure, .actorprefix:
+                return true
+            default:
+                return false
+        }
+    }
 }
