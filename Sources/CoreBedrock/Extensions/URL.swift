bd67ee950b8df7b3bb0c2c53fc28be98e6310df1
yechentide <yechentide@gmail.com>
2023-07-29 22:58:05 +0900

    refac: change access modifier

--- Sources/CoreBedrock/Extensions/URL.swift
+++ Sources/CoreBedrock/Extensions/URL.swift
@@ -4,14 +4,14 @@ import Foundation
 extension URL {
     private static let byteCountFormatter = ByteCountFormatter()
 
-    func isDirectoryAndReachable() throws -> Bool {
+    public func isDirectoryAndReachable() throws -> Bool {
         guard try resourceValues(forKeys: [.isDirectoryKey]).isDirectory == true else {
             return false
         }
         return try checkResourceIsReachable()
     }
 
-    func directorySize() throws -> UInt? {
+    public func directorySize() throws -> UInt? {
         guard try isDirectoryAndReachable(),
               let urls = FileManager.default.enumerator(at: self, includingPropertiesForKeys: nil)?.allObjects as? [URL]
         else {
@@ -23,7 +23,7 @@ extension URL {
         return UInt(rawSize)
     }
 
-    func formattedDirectorySize() throws -> String? {
+    public func formattedDirectorySize() throws -> String? {
         guard let rawSize = try self.directorySize() else {
             return nil
         }
