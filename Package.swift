4e7af642d405e824285daaaf6ebffd7606480f40
yechentide <yechentide@gmail.com>
2024-07-28 17:11:05 +0900

    fix: Logger cannot be used under iOS 14

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CoreBedrock",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v14),
         .macOS(.v11),
     ],
     products: [
