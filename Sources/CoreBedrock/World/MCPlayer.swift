9b5cceb8a9a623fcb940ca3f3cf994b5eebdb589
yechentide <yechentide@gmail.com>
2024-10-04 22:38:36 +0900

    feat: add MCPlayer struct to represent player IDs

---
+++ Sources/CoreBedrock/World/MCPlayer.swift
@@ -0,0 +1,17 @@
+//
+// Created by yechentide on 2024/10/04
+//
+
+public struct MCPlayer: Equatable, Hashable {
+    public let msaID: String       // player_xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+    public let signedID: String    // player_xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+    public let serverID: String    // player_server_xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+
+    public static func == (lhs: MCPlayer, rhs: MCPlayer) -> Bool {
+        return lhs.msaID == rhs.msaID
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(msaID)
+    }
+}
