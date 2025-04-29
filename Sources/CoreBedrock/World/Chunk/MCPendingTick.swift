877b28d3bceb37cc2f48ab0ebfe2cabc72e15279
yechentide <yechentide@gmail.com>
2025-04-29 18:38:56 +0900

    feat: add MCPendingTick

---
+++ Sources/CoreBedrock/World/Chunk/MCPendingTick.swift
@@ -0,0 +1,30 @@
+//
+// Created by yechentide on 2025/04/19
+//
+
+public struct MCPendingTick {
+    var x: Int32
+    var y: Int32
+    var z: Int32
+    var time: Int64
+    let block: MCBlock
+
+    public static func parse(rootTag: CompoundTag) -> MCPendingTick? {
+        guard let blockStateTag = rootTag["blockState"] as? CompoundTag,
+              let block = MCBlock.decode(blockStateTag),
+              let timeTag = rootTag["time"] as? LongTag,
+              let xTag = rootTag["x"] as? IntTag,
+              let yTag = rootTag["y"] as? IntTag,
+              let zTag = rootTag["z"] as? IntTag
+        else {
+            return nil
+        }
+        return MCPendingTick(
+            x: xTag.value,
+            y: yTag.value,
+            z: zTag.value,
+            time: timeTag.value,
+            block: block
+        )
+    }
+}
