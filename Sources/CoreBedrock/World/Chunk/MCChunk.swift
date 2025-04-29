1ca4475ef8759d9fb408de6f71e5e3bee3732c73
yechentide <yechentide@gmail.com>
2025-04-29 18:39:47 +0900

    feat: expand MCChunk with full structure and add MCChunkReadOptions

--- Sources/CoreBedrock/World/Chunk/MCChunk.swift
+++ Sources/CoreBedrock/World/Chunk/MCChunk.swift
@@ -6,9 +6,42 @@ import CoreGraphics
 import LvDBWrapper
 
 public struct MCChunk {
-    public static let length = 16
-    public static var viewSize: Int { length * length }
+    public static var viewSize: Int { MCSubChunk.sideLength * MCSubChunk.sideLength }
     public static func calcRange(chunkIndex: Int32) -> ClosedRange<Int> {
-        return Int(chunkIndex)*length ... Int(chunkIndex+1)*length-1
+        return Int(chunkIndex)*MCSubChunk.sideLength ... Int(chunkIndex+1)*MCSubChunk.sideLength-1
+    }
+
+    let loadOption: MCChunkReadOptions
+
+    let chunkX: Int32
+    let chunkZ: Int32
+    var minChunkY: Int32
+
+    let version: UInt8
+    var currentTikc: Int32 = 0
+
+    var subChunks: [MCSubChunk]
+    var blockEntities: [Pos3Di32: CompoundTag]
+    var entities: [CompoundTag]
+    var pendingTicks: [MCPendingTick]
+    var biomes: MCBiomeColumn
+
+    var minBlockX: Int {
+        Int(chunkX) * 16
+    }
+    var maxBlockX: Int {
+        Int(chunkX) * 16 + 15
+    }
+    var minBlockZ: Int {
+        Int(chunkZ) * 16
+    }
+    var maxBlockZ: Int {
+        Int(chunkZ) * 16 + 15
+    }
+    var minBlockY: Int {
+        Int(minChunkY) * 16
+    }
+    var maxBlockY: Int {
+        minChunkY < 0 ? 319 : 255
     }
 }
---
+++ Sources/CoreBedrock/World/Chunk/MCChunkReadOptions.swift
@@ -0,0 +1,23 @@
+//
+// Created by yechentide on 2025/04/19
+//
+
+public struct MCChunkReadOptions: OptionSet, Sendable {
+    public let rawValue: Int
+
+    public init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+
+    public static let biome         = MCChunkReadOptions(rawValue: 1 << 0)
+    public static let pendingTicks  = MCChunkReadOptions(rawValue: 1 << 1)
+    public static let blockEntities = MCChunkReadOptions(rawValue: 1 << 2)
+    public static let entities      = MCChunkReadOptions(rawValue: 1 << 3)
+
+    public static let all: MCChunkReadOptions = [
+        .biome,
+        .pendingTicks,
+        .blockEntities,
+        .entities
+    ]
+}
